043f451f55ef414f52b3bc54b55da8e07b980e2c
Soroush Khosravi <sorush.kh68@gmail.com>
2019-05-13 12:06:48 +0100

    The post redirect loop is used inside the application now.

--- app.rb
+++ app.rb
@@ -1,15 +1,24 @@
 require 'sinatra/base'
 require_relative "./lib/player.rb"
+
 class Battle < Sinatra::Base
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
   post '/play' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
+  end
+
+  get '/play' do
     erb :game
   end
+
+
 
 
   run! if app_file == $0
